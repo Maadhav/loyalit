46abbfb3b9a5c089130ae393b5fc32f99622ace4
Maadhav Sharma <maadhav2001@gmail.com>
2023-03-22 10:19:34 +0530

    merge complete

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,7 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
